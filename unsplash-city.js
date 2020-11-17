bb7cd13f5cb96b9980a703bc57007bbf275cc457
FN510 <f.ngemoh@warwick.ac.uk>
2020-11-17 13:57:19 +0000

    update home

--- unsplash-city.js
+++ unsplash-city.js
@@ -1,5 +1,5 @@
 const apiKey = 'f83086e9fd82d63cb1d8f83672ba1d2375aa88b69280b31a80ec43a6a0f373f3';
-const collection_id = '858902'
+const collection_id = '1202574'
 fetch('https://api.unsplash.com/collections/'+ collection_id + '/photos/?client_id='+ apiKey+'&per_page=50')
   .then((res) => res.json()) // Transform the data into json
   .then(function(data) {
@@ -9,7 +9,7 @@ fetch('https://api.unsplash.com/collections/'+ collection_id + '/photos/?client_
   	// write image grid HTML into DOM
   	for (var i=0; i<data.length; i++) {
   		document.getElementsByClassName('row')[0].innerHTML +=
-       `<div class="col-lg-4 col-sm-6"><div class="thumbnail"><a  target="_blank" href=` + data[i].links.html + `><img src= `+data[i].urls.small + `></a>`;  	
+       `<div class="col-lg-4 col-sm-6"><div class="thumbnail"><a  target="_blank" href=` + data[i].links.html + `><img class="gallery" src= `+data[i].urls.small + `></a>`;  	
   	}
     })
   		
